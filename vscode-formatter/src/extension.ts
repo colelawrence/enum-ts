f32f7c60201765d580a8709eadcf81ae2b888b65
Cole Lawrence <cole@colelawrence.com>
2021-01-29 22:57:13 -0500

    Add automatic folding of enum regions

--- vscode-formatter/src/extension.ts
+++ vscode-formatter/src/extension.ts
@@ -23,14 +23,67 @@ export function activate(context: vscode.ExtensionContext) {
         },
         document
       ).then((enumEdit) => {
-        if (enumEdit && enumEdit.version === document.version) {
-          const edit = new vscode.WorkspaceEdit();
-          edit.replace(document.uri, enumEdit.range, enumEdit.replacement);
-          return vscode.workspace.applyEdit(edit);
+        if (enumEdit) {
+          if (enumEdit.version === document.version) {
+            const edit = new vscode.WorkspaceEdit();
+            edit.replace(document.uri, enumEdit.range, enumEdit.replacement);
+            return vscode.workspace.applyEdit(edit);
+          } else {
+            vscode.window.showErrorMessage("Document changed while formatting");
+          }
+        } else {
+          vscode.window.showInformationMessage(
+            "Already up-to-date based on hash. Delete regions to regenerate."
+          );
         }
       });
     }
   });
+
+  const REGION_TO_FOLD = /#region enum-ts generated/g;
+  vscode.window.onDidChangeActiveTextEditor(async (textEditor) => {
+    const document = textEditor.document;
+    if (langSet.has(document.languageId)) {
+      const text = document.getText();
+      let match: RegExpExecArray;
+      const prevSelection = textEditor.selection;
+      while (((match = REGION_TO_FOLD.exec(text)), match != null)) {
+        const start = document.positionAt(match.index);
+        // based on https://github.com/eramdam/fold-imports/blob/1fcd5a37c9d53749379d13747b9ac27660c4712e/src/helpers.ts#L82
+        await vscode.commands.executeCommand("editor.fold", {
+          levels: 1,
+          selectionLines: [start.line],
+        });
+      }
+      textEditor.selection = prevSelection;
+    }
+  });
+  // vscode.workspace.onDidOpenTextDocument(async (document) => {
+  //   const textEditor = vscode.window.visibleTextEditors.find(
+  //     (visibleTextEditor) => visibleTextEditor.document === document
+  //   );
+  //   if (!textEditor) return;
+  //   vscode.window.showInformationMessage(
+  //     "Automatically folding generated enum"
+  //   );
+  //   if (langSet.has(document.languageId)) {
+  //     const text = document.getText();
+  //     let match: RegExpExecArray;
+  //     const prevSelection = textEditor.selection;
+  //     while (((match = REGION_TO_FOLD.exec(text)), match != null)) {
+  //       const start = document.positionAt(match.index);
+  //       const end = document.positionAt(match.index + match[0].length);
+  //       textEditor.selection = new vscode.Selection(
+  //         start.line,
+  //         start.character,
+  //         end.line,
+  //         end.character
+  //       );
+  //       await vscode.commands.executeCommand("editor.fold");
+  //     }
+  //     textEditor.selection = prevSelection;
+  //   }
+  // });
 
   // 👍 formatter implemented using API
   vscode.languages.registerDocumentFormattingEditProvider(langs, {
